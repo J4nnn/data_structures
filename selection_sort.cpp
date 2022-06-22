b0a41befff289b9141a728c12dc3c89f4dbf8901
Janer Vega <janervega399@gmail.com>
2022-06-22 15:50:41 -0500

    Implementación en proceso

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
 int main() {
-    int min, vector[15] = {14, 76, 20, 11, 27, 64, 36, 54, 89, 12, 78, 92, 44, 33, 75};
-    min = vector[0];
+    int min, aux, vector[15] = {14, 76, 20, 11, 27, 64, 36, 54, 89, 12, 78, 92, 44, 33, 75};
     for (int i = 0; i < sizeof(vector)/sizeof(int); i++) {
-        if (vector[i] > vector[i + 1]) {
-            min = vector[i + 1];
+        min = vector[i];
+        for (int j = 0; j < sizeof(vector)/sizeof(int) - 1; j++) {
+            if (vector[j + 1] < min) {
+                min = vector[j + 1];
+            }
         }
+        aux = min;
     }
-    std::cout << min << std::endl;
-    std::cout << sizeof(vector)/sizeof(int) << std::endl;
+    std::cout << sizeof(vector)/sizeof(int) - 1 << std::endl;
     return 0;
 }
