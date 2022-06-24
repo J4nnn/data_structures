c7fd601ab275081bffaf39ca6e575df56b93d9ee
Janer Vega <janervega399@gmail.com>
2022-06-24 11:44:05 -0500

    La cagué aquí :(

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -4,13 +4,20 @@ int main() {
     int min, aux, vector[15] = {14, 76, 20, 11, 27, 64, 36, 54, 89, 12, 78, 92, 44, 33, 75};
     for (int i = 0; i < sizeof(vector)/sizeof(int); i++) {
         min = vector[i];
-        for (int j = 0; j < sizeof(vector)/sizeof(int) - 1; j++) {
+        for (int j = 0; j < sizeof(vector)/sizeof(int); j++) {
             if (vector[j + 1] < min) {
                 min = vector[j + 1];
             }
         }
-        aux = min;
+        aux = vector[i + 1];
+        vector[i + 1] = vector[i];
+        vector[i] = aux;
     }
-    std::cout << sizeof(vector)/sizeof(int) - 1 << std::endl;
+
+    std::cout << "El vector ordenado es: " << std::endl;
+    for (int i = 0; i < sizeof(vector)/sizeof(int); i++) {
+        std::cout << vector[i] << " " << std::endl;
+    }
+    
     return 0;
 }
