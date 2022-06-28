fdcf7499150174844e4923653fa890d1f80e6888
Janer Vega <janervega399@gmail.com>
2022-06-27 22:18:41 -0500

    totally different algorithm

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 
-int main() {
-    int min, aux, vector[15] = {14, 76, 20, 11, 27, 64, 36, 54, 89, 12, 78, 92, 44, 33, 75};
-    for (int i = 0; i < sizeof(vector)/sizeof(int); i++) {
-        min = vector[i];
-        for (int j = 0; j < sizeof(vector)/sizeof(int); j++) {
-            if (vector[j + 1] < min) {
-                min = vector[j + 1];
-            }
-        }
-        aux = vector[i + 1];
-        vector[i + 1] = vector[i];
-        vector[i] = aux;
-    }
+void swap (int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
 
-    std::cout << "El vector ordenado es: " << std::endl;
-    for (int i = 0; i < sizeof(vector)/sizeof(int); i++) {
-        std::cout << vector[i] << " " << std::endl;
+void display (int *array) {
+    for (int i = 0; i < sizeof(array)/sizeof(int); i++) {
+        std::cout << array[i] << " ";
     }
-    
+    std::cout << std::endl;
+}
+
+void selection_sort (int *array) {
+    int i, j, min;
+    for (int i = 0; i < sizeof(array)/sizeof(int); i++) {
+        min = i;
+        for (int j = i + 1; j < sizeof(array)/sizeof(int) - 1; j++) {
+            if (array[j] < array[min]) {
+                min = j;
+            }
+            swap(array[i], array[min]);
+        }
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    int array[10] = {9, 4, 7, 1, 8, 0, 5, 6, 2, 3};
+    std::cout << "Before sorting:";
+    display(array);
+    selection_sort(array);
+    std::cout << "After sortin:";
+    display(array);
     return 0;
 }
